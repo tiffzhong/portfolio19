0bd2f8549641ef7122bcce02a0bace5a330c2e95
Tiffany Zhong <tiffzhong@gmail.com>
2019-04-20 16:47:26 -0700

    education section

--- src/education/Education.js
+++ src/education/Education.js
@@ -10,7 +10,43 @@ const Education = () => {
         </div>
 
         <div className="right-education">
-          <img src="" />
+          <section>
+            <div className="subheading">
+              <h4>DevMountain</h4> - <p>Phoenix, AZ</p>
+            </div>
+            <div className="subheading-2">
+              <h6>Web Development</h6>
+            </div>
+            <p>
+              12 Week Immersive Full Stack Web Development & Software
+              Engineering Program
+            </p>
+          </section>
+
+          <section>
+            <div className="subheading">
+              <h4>San Jose State University</h4> - <p>San Jose, CA</p>
+              <p>August 2011 - May 2015</p>
+            </div>
+            <div className="subheading-2">
+              <h6>
+                B.S. in Health Science, Health Service Administration; Minor:
+                Business
+              </h6>
+            </div>
+            <p>GPA: 3.75</p>
+          </section>
+
+          <section>
+            <div className="subheading">
+              <h4>Edinburgh Napier University</h4> - <p>Edinburgh, UK</p>
+              <p>August 2013 - January 2014</p>
+            </div>
+            <div className="subheading-2">
+              <h6>Study Abroad, Bilateral Program</h6>
+            </div>
+            <p>Course Focus: Business Marketing</p>
+          </section>
         </div>
       </div>
     </div>
--- src/experience/Experience.js
+++ src/experience/Experience.js
@@ -10,10 +10,23 @@ const Experience = () => {
         </div>
 
         <div className="right-experience">
-          <div className="yelp">
+          <section>
             <h4>Yelp, Inc.</h4> - <p>San Francisco, CA</p>
             <p>Facilities Coordinator</p>
-          </div>
+            <h6>February 2016 - May 2018</h6>
+            <ul>
+              <li>
+                • Handled the Ergonomics program by creating a new policy,
+                performed 35+ employee evaluations per quarter, created an
+                ergonomic video, and provided proper workstation equipment
+                (desk, chair, keyboard, mouse, etc.)
+              </li>
+              <li>
+                • Oversaw the Facilities ticketing system (JIRA) and closed out
+                15+ trouble tickets weekly
+              </li>
+            </ul>
+          </section>
         </div>
       </div>
     </div>
