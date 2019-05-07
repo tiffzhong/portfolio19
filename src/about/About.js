3bb0c8eb0438851e8011d819050671f29e4ab63b
Tiffany Zhong <tiffzhong@gmail.com>
2019-05-07 15:28:10 -0700

    about changes

--- src/about/About.js
+++ src/about/About.js
@@ -11,15 +11,17 @@ const About = () => {
 
         <div className="right-about">
           <div>
-            Born and raised in the Bay Area, SJSU graduate, and self-taught
-            programmer. Strong interest in full-stack development focusing on
+            Born and raised in the Bay Area, first generation Asian American,
+            SJSU graduate, and self-taught programmer. I am passionate about
+            building tools to make people move faster, live easier and work more
+            efficiently. Strong interest in full-stack development focusing on
             well designed, clean, responsive, and scalable code.
-          </div>{" "}
+          </div>
           <div>
             A quick learner, self starter, innovative, and creative. I have a
             love for mountains, music, and movies. When I'm not coding you can
-            find me eating carbs, lifting at the gym, running, traveling,
-            binging the latest Netflix movie, or taking pictures of my cat.
+            find me eating carbs, at the gym, running, traveling, binging the
+            latest Netflix series, or taking pictures of my cat.
           </div>
         </div>
       </div>
